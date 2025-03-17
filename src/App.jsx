56940197807e3e1bddab5ca68856ed0e1c4790e2
ajithreddy1441 <syamalaajith11@gmail.com>
2025-03-17 16:23:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BackgroundGradientAnimationDemo } from './test'
+
+function App() {
+  return (
+    <>
+    
+    <BackgroundGradientAnimationDemo />
+    </>
+  )
+}
+
+export default App
